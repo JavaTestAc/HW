1e09a4a2746ec026ff8918d762531bcec800011a
JavaTestAc <aleksandr.g0nchar@yandex.ru>
2016-09-24 21:20:32 +0300

    HW Simple data types

---
+++ SimpleDataTypes.java
@@ -0,0 +1,16 @@
+/**
+ * Created by AG on 24.09.2016.
+ */
+public class SimpleDataTypes {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!!!");
+
+        String s = "First part" +
+                " Second part" +
+                " Fird part";
+
+        System.out.print(s);
+    }
+}
